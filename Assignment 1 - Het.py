e8cc6205eea9353e555836f8b92464f1197a4852
hetr7999 <57358161+hetr7999@users.noreply.github.com>
2021-03-10 22:11:25 +0530

    Add files via upload

---
+++ Assignment 1 - Het.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+x = int(input("Enter x value"))
+y = int(input("Enter y value"))
+
+z = pow(x,y)
+
+print(z)
+
+
+# In[23]:
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+for x in numbers:
+    if (x%3 == 0) :
+        print(x)
+    else:
+        print("")
+
+
+# In[22]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
